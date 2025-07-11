710656d86e5072319d20fcf358a1ee093e063619
P Rohith Raveendran <150608923+rohithr2511@users.noreply.github.com>
2025-07-11 21:00:45 +0530

    Update tracker.py

--- trackers/_pycache_/tracker.py
+++ trackers/_pycache_/tracker.py
@@ -1,4 +1,4 @@
-#Import All the Required Libraries
+#ImportingLibraries
 import cv2
 import os
 import numpy as np
@@ -6,47 +6,45 @@ import pickle
 from ultralytics import YOLO
 import supervision as sv
 from utils import get_center_of_bbox, get_bbox_width
+
 class Tracker:
     def __init__(self, model_path):
         self.model = YOLO(model_path)
         self.tracker = sv.ByteTrack()
+
     def detect_frames(self, frames):
         batch_size = 20
         detections = []
         for i in range(0, len(frames), batch_size):
-            detections_batch = self.model.predict(frames[i: i + batch_size], conf = 0.1)
+            detections_batch = self.model.predict(frames[i: i + batch_size], conf=0.1)
             detections += detections_batch
         return detections
+
     def get_object_tracks(self, frames, read_from_stub=False, stub_path=None):
         if read_from_stub and stub_path is not None and os.path.exists(stub_path):
-            with open(stub_path,'rb') as f:
+            with open(stub_path, 'rb') as f:
                 tracks = pickle.load(f)
             return tracks
 
         detections = self.detect_frames(frames)
         tracks = {
-            "players": [],
-            "referees": [],
+            "football_players": [],
+            "refrees": [],
             "ball": []
         }
 
         for frame_num, detection in enumerate(detections):
             cls_names = detection.names
-            cls_names_inv = {v:k for k, v in cls_names.items()}
+            cls_names_inv = {v: k for k, v in cls_names.items()}
 
-            #Convert to Supervision Detection Format
+            # Convert to Supervision Detection Format
             detection_supervision = sv.Detections.from_ultralytics(detection)
 
-            #Convert GoalKeeper to Player Object
-            for object_ind, class_id in enumerate(detection_supervision.class_id):
-                if cls_names[class_id] == "goalkeeper":
-                    detection_supervision.class_id[object_ind] = cls_names_inv["player"]
-
-            #Track Objects
+            # Track Objects
             detection_with_tracks = self.tracker.update_with_detections(detection_supervision)
 
-            tracks["players"].append({})
-            tracks["referees"].append({})
+            tracks["football_players"].append({})
+            tracks["refrees"].append({})
             tracks["ball"].append({})
 
             for frame_detection in detection_with_tracks:
@@ -54,21 +52,21 @@ class Tracker:
                 cls_id = frame_detection[3]
                 track_id = frame_detection[4]
 
-                if cls_id == cls_names_inv['player']:
-                    tracks["players"][frame_num][track_id] = {"bbox": bbox}
-                if cls_id == cls_names_inv['referee']:
-                    tracks["referees"][frame_num][track_id] = {"bbox":bbox}
+                if "football players" in cls_names_inv and cls_id == cls_names_inv["football players"]:
+                    tracks["football_players"][frame_num][track_id] = {"bbox": bbox}
+                if "refree" in cls_names_inv and cls_id == cls_names_inv["refree"]:
+                    tracks["refrees"][frame_num][track_id] = {"bbox": bbox}
 
             for frame_detection in detection_supervision:
                 bbox = frame_detection[0].tolist()
                 cls_id = frame_detection[3]
 
-                if cls_id == cls_names_inv['ball']:
-                    tracks["ball"][frame_num][1] = {"bbox":bbox}
+                if "football" in cls_names_inv and cls_id == cls_names_inv["football"]:
+                    tracks["ball"][frame_num][1] = {"bbox": bbox}
 
         if stub_path is not None:
-            with open(stub_path,'wb') as f:
-                pickle.dump(tracks,f)
+            with open(stub_path, 'wb') as f:
+                pickle.dump(tracks, f)
 
         return tracks
 
@@ -138,19 +136,19 @@ class Tracker:
         for frame_num, frame in enumerate(video_frames):
             frame = frame.copy()
 
-            player_dict = tracks["players"][frame_num]
+            player_dict = tracks["football_players"][frame_num]
             ball_dict = tracks["ball"][frame_num]
-            referee_dict = tracks["referees"][frame_num]
+            refree_dict = tracks["refrees"][frame_num]
 
-            # Draw Players
+            # Draw Football Players
             for track_id, player in player_dict.items():
-                frame = self.draw_ellipse(frame, player["bbox"], (0,0,255), track_id)
+                frame = self.draw_ellipse(frame, player["bbox"], (0, 0, 255), track_id)
 
-            # Draw Referee
-            for _, referee in referee_dict.items():
-                frame = self.draw_ellipse(frame, referee["bbox"], (0, 255, 255))
+            # Draw Refree
+            for _, refree in refree_dict.items():
+                frame = self.draw_ellipse(frame, refree["bbox"], (0, 255, 255))
 
-            # Draw ball
+            # Draw Football
             for track_id, ball in ball_dict.items():
                 frame = self.draw_traingle(frame, ball["bbox"], (0, 255, 0))
 
